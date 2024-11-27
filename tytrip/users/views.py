34cc4a218cd0a8b1689ec127fc847e92c1f80302
DTigi <dtiginyanu@gmail.com>
2024-11-27 14:12:51 +0300

    add PasswordChangeView add PasswordChangeDoneView, templates and forms

--- tytrip/users/views.py
+++ tytrip/users/views.py
@@ -1,13 +1,13 @@
 from django.contrib.auth import authenticate, login, logout, get_user_model
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, PasswordChangeView
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse, reverse_lazy
 from django.views.generic import CreateView, UpdateView
 
-from .forms import LoginUserForm, RegisterUserForm, ProfileUserForm
+from .forms import LoginUserForm, RegisterUserForm, ProfileUserForm, UserPasswordChangeForm
 
 
 # Create your views here.
@@ -27,7 +27,7 @@ class RegisterUser(CreateView):
 
 
 class ProfileUser(LoginRequiredMixin, UpdateView):
-    model = get_user_model()
+    # model = get_user_model()
     form_class = ProfileUserForm
     template_name = 'users/profile.html'
     extra_context = {'title': "Профиль пользователя"}
@@ -38,6 +38,14 @@ class ProfileUser(LoginRequiredMixin, UpdateView):
 
     def get_object(self, queryset=None):
         return self.request.user
+
+
+class UserPasswordChange(PasswordChangeView):
+    form_class = UserPasswordChangeForm
+    success_url = reverse_lazy("users:password_change_done")
+    template_name = "users/password_change_form.html"
+    extra_context = {'title': "Изменение пароля"}
+
 
 ####################### func_views ######################################
 # def login_user(request):
